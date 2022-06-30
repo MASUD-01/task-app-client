53fe8fb81edef5a41720d7f5f9dba2cc0d9fb1fe
MASUD-01 <masudrana96141@gmail.com>
2022-07-01 03:15:50 +0600

    add

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 
 const Todo = () => {
     const [tasks, setTask] = useState([])
@@ -12,8 +13,9 @@ const Todo = () => {
         <div className='wide'>
             <h1> Your Todo</h1>
 
+
             {
-                tasks.map(task => <li key={task._id}>{task.tas}</li>)
+                tasks.map(task => <li key={task._id}>{task.tas} <Link to={`/update/${task._id}`}><button className="btn btn-xs">Edit</button></Link></li>)
             }
         </div>
     );
